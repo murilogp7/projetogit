11e05d237f54f3812746852de44f41ab83e0ea78
murilogp7 <165444504+murilogp7@users.noreply.github.com>
2025-06-21 23:47:05 -0300

    validação.js

--- validação.js
+++ validação.js
@@ -1,5 +1,17 @@
-let. username = document.getElementById("username")
+let. form = document.getElementById("form")
+let.  username = document.getElementById("username")
 let. email = document.getElementById("email")
 let.  password = document.getElementById("password")
 
+form.addEventlistener("submit",(e) => {
+   if(username.value) {
+    console.log("aba preenchida");
+   }
+   else {
+    console.log("aba vazia");
+   }
 
+    e.preventDefault();
+
+
+} )
